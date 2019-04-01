6d759d92eaeae480f8805404ccbdc1c154100094
Rob <bruno.robsiq@gmail.com>
2019-04-01 14:03:10 -0300

    Improve tests

--- R201/src/test/java/sorting/test/StudentSortingTest.java
+++ R201/src/test/java/sorting/test/StudentSortingTest.java
@@ -16,17 +16,14 @@ public class StudentSortingTest {
 	private Integer[] vetorVazio = {};
 	private Integer[] vetorValoresRepetidos;
 	private Integer[] vetorValoresIguais;
+	private Integer[] vetorUmElemento;
+	private Integer[] vetorDoisElementos;
 	private Integer[] grandeRandom;
 
 	public AbstractSorting<Integer> implementation;
 
 	@Before
 	public void setUp() {
-		Integer[] arrayRandom = new Integer[20];
-
-		for (int i = 0; i < arrayRandom.length; i++) {
-			arrayRandom[i] = (Integer) Math.ceil(Math.random() * 100);
-		}
 
 
 		populaVetorTamanhoPar(new Integer[] { 30, 28, 7, 29, 11, 26, 4, 22, 23,
@@ -35,6 +32,15 @@ public class StudentSortingTest {
 				11, 18, 36 });
 		populaVetorRepetido(new Integer[] { 4, 9, 3, 4, 0, 5, 1, 4 });
 		populaVetorIgual(new Integer[] { 6, 6, 6, 6, 6, 6 });
+		popularVetorUm(new Integer[]{0});
+		populaVetorDois(new Integer[]{1, 0});
+
+		Integer[] arrayRandom = new Integer[20];
+
+		for (int i = 0; i < arrayRandom.length; i++) {
+			arrayRandom[i] = (int) Math.round(Math.random() * 100);
+		}
+		populaRandom(arrayRandom);
 
 		getImplementation();
 	}
@@ -63,6 +69,21 @@ public class StudentSortingTest {
 
 	public void populaVetorIgual(Integer[] arrayPadrao) {
 		this.vetorValoresIguais = Arrays
+				.copyOf(arrayPadrao, arrayPadrao.length);
+	}
+
+	public void popularVetorUm(Integer[] arrayPadrao) {
+		this.vetorUmElemento = Arrays
+				.copyOf(arrayPadrao, arrayPadrao.length);
+	}
+
+	public void populaVetorDois(Integer[] arrayPadrao) {
+		this.vetorDoisElementos = Arrays
+				.copyOf(arrayPadrao, arrayPadrao.length);
+	}
+
+	public void populaRandom(Integer[] arrayPadrao) {
+		this.grandeRandom = Arrays
 				.copyOf(arrayPadrao, arrayPadrao.length);
 	}
 
@@ -105,6 +126,14 @@ public class StudentSortingTest {
 		genericTest(vetorValoresRepetidos);
 	}
 
+	@Test
+	public void testSort06() { genericTest(grandeRandom); }
+
+	@Test
+	public void testSort07() { genericTest(vetorDoisElementos); }
+
+	@Test
+	public void testSort08() { genericTest(vetorUmElemento); }
 	// MÉTODOS QUE OS ALUNOS PODEM CRIAR
 	/**
 	 * O ALUNO PODE IMPLEMENTAR METODOS DE ORDENAÇÃO TESTANDO O SORT COM TRES
