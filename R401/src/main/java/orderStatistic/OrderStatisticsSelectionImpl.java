dc9d1a98bd5aff427b9a93b6a01deba06149cf9d
Rob <bruno.robsiq@gmail.com>
2019-04-08 17:29:26 -0300

    Add OrderStatisticsSelection

--- R401/src/main/java/orderStatistic/OrderStatisticsSelectionImpl.java
+++ R401/src/main/java/orderStatistic/OrderStatisticsSelectionImpl.java
@@ -21,8 +21,73 @@ public class OrderStatisticsSelectionImpl<T extends Comparable<T>> implements Or
 	 */
 	@Override
 	public T getOrderStatistics(T[] array, int k) {
-		//TODO implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+	  return selectMin(array, k);
 	}
 
+	// ======================
+	// TODO: REMOVE
+	public static void main(String[] args) {
+		Integer[] a = new Integer[]{3,-7,-4,2,5,1};
+		OrderStatistics<Integer> test = new OrderStatisticsSelectionImpl<>();
+
+		System.out.println(test.getOrderStatistics(a, 0));
+		System.out.println(test.getOrderStatistics(a, 1));
+		System.out.println(test.getOrderStatistics(a, 2));
+		System.out.println(test.getOrderStatistics(a, 3));
+		System.out.println(test.getOrderStatistics(a, 4));
+		System.out.println(test.getOrderStatistics(a, 5));
+		System.out.println(test.getOrderStatistics(a, 6));
+		System.out.println(test.getOrderStatistics(a, 7));
+	}
+	// =====================
+
+
+	private T selectMin(T[] array, int k) {
+		T min = null;
+		if (k > 0 && k <= array.length) {
+			min = array[0];
+
+			for (int i = 1; i < array.length; i++) {
+				min = this.min(min, array[i]);
+			}
+
+			min = selectMin(array, k - 1, min);
+		}
+
+		return min;
+	}
+
+	private T selectMin(T[] array, int k, T min) {
+		if (k > 0) {
+		  T nextMin = null;
+
+			for (T element : array) {
+				if (element != null && element.compareTo(min) > 0) {
+				  if (nextMin == null) {
+				  	nextMin = element;
+					} else {
+						nextMin = this.min(element, nextMin);
+					}
+				}
+			}
+
+			min = selectMin(array, k - 1, nextMin);
+		}
+
+
+		return min;
+	}
+
+	private boolean isValidSearch(T[] array, int k) {
+		return array.length >= k;
+	}
+
+	private T min(T a, T b) {
+		T min = a;
+		if (b.compareTo(a) < 0) {
+			min = b;
+		}
+
+		return min;
+	}
 }
