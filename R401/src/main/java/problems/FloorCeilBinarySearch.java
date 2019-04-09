bba27c3c35ae56faac74721c22c483e276d9549f
Rob <bruno.robsiq@gmail.com>
2019-04-08 23:30:27 -0300

    Fix arg order on FloorCeil

--- R401/src/main/java/problems/FloorCeilBinarySearch.java
+++ R401/src/main/java/problems/FloorCeilBinarySearch.java
@@ -1,7 +1,5 @@
 package problems;
 
-import java.util.Arrays;
-
 /**
  * Calcula o floorOrCeil e ceil de um numero em um array usando a estrategia de busca
  * binaria.
@@ -16,41 +14,15 @@ import java.util.Arrays;
 public class FloorCeilBinarySearch implements FloorCeil {
 	@Override
 	public Integer floor(Integer[] array, Integer x) {
-		return floorOrCeil(array, 0, array.length - 1, x, 1);
+		return floorOrCeil(array, x, 0, array.length - 1, 1);
 	}
 
 	@Override
 	public Integer ceil(Integer[] array, Integer x) {
-		return floorOrCeil(array, 0, array.length - 1, x, 0);
+		return floorOrCeil(array, x, 0, array.length - 1, 0);
 	}
 
-	/**
-	 * Floor or ceil algorithm by checking where in the two partitions a smaller element is located.
-	 * By checking it on the first partition, we proceed the recursive search on the second,
-	 * effectively doing a ceil function (all elements on second are guaranteed to be no smaller than x).
-     *
-	 * [4 6 8 10], ceil 7
-	 * [4 6] [8 10]
-	 *    ^ (smaller, so proceed with the second)
-	 *      [8] [10]
-	 *       ^ (bigger, so get the first partition)
-	 *
-	 *      8 (smallest element bigger than 7)
-	 *
-	 * If we try and find it on the second partition and proceed the recursive search on the first,
-	 * we do a floor function (all elements on first partition are guaranteed to be no bigger than x).
-     *
-	 * [4 6 8 10], floor 7
-	 * [4 6] [8 10]
-	 *        ^ (bigger, so get the first partition)
-	 *
-	 * [4] [6]
-	 *      ^ (smaller, so proceed with the second)
-	 *
-	 *      6 (biggest element smaller than 7)
-	 *
-	 */
-	private Integer floorOrCeil(Integer[] array, int leftIndex, int rightIndex, Integer x, int compareOffset) {
+	private Integer floorOrCeil(Integer[] array, Integer x, int leftIndex, int rightIndex, int compareOffset) {
 		Integer result = null;
 
 		if (leftIndex == rightIndex) {
@@ -61,16 +33,16 @@ public class FloorCeilBinarySearch implements FloorCeil {
 			if (array[med].equals(x) || array[med + 1].equals(x)) {
 				result = x;
 			} else if (array[med + compareOffset].compareTo(x) < 0) {
-				result = floorOrCeil(array, med + 1, rightIndex, x, compareOffset);
+				result = floorOrCeil(array, x, med + 1, rightIndex, compareOffset);
 			} else {
-				result = floorOrCeil(array, leftIndex, med, x, compareOffset);
+				result = floorOrCeil(array, x, leftIndex, med, compareOffset);
 			}
 		}
 
 		return result;
 	}
 
-	private boolean searchable(Integer[] array, int leftIndex, int righIndex) {
-		return leftIndex >= 0 && righIndex < array.length && leftIndex < righIndex;
+	private boolean searchable(Integer[] array, int leftIndex, int rightIndex) {
+		return leftIndex >= 0 && rightIndex < array.length && leftIndex < rightIndex;
 	}
 }
