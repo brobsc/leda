043263e583707050406d99e1e1510e0a3c97ad4f
Rob <bruno.robsiq@gmail.com>
2019-04-08 21:40:57 -0300

    Refactor FloorCeil to only one method

--- R401/src/main/java/problems/FloorCeilBinarySearch.java
+++ R401/src/main/java/problems/FloorCeilBinarySearch.java
@@ -1,7 +1,9 @@
 package problems;
 
+import java.util.Arrays;
+
 /**
- * Calcula o floor e ceil de um numero em um array usando a estrategia de busca
+ * Calcula o floorOrCeil e ceil de um numero em um array usando a estrategia de busca
  * binaria.
  *
  * Restricoes: 
@@ -12,55 +14,17 @@ package problems;
  *
  */
 public class FloorCeilBinarySearch implements FloorCeil {
-
 	@Override
 	public Integer floor(Integer[] array, Integer x) {
-		return floor(array, 0, array.length - 1, x);
-	}
-
-	public static void main(String[] args) {
-		Integer[] a = new Integer[]{4,6,6,6,8,10};
-
-		FloorCeilBinarySearch f = new FloorCeilBinarySearch();
-
-		System.out.println(f.floor(a, 7));
-		System.out.println(f.ceil(a, 7));
-		System.out.println(f.floor(a, 8));
-		System.out.println(f.ceil(a, 8));
-		System.out.println(f.floor(a, 12));
-		System.out.println(f.ceil(a, 12));
-		System.out.println(f.floor(a, 2));
-		System.out.println(f.ceil(a, 2));
-		System.out.println(f.floor(a, 5));
-		System.out.println(f.ceil(a, 5));
-	}
-
-	private Integer floor(Integer[] array, int leftIndex, int rightIndex, Integer x) {
-		Integer result = null;
-
-		if (leftIndex == rightIndex) {
-			result = array[leftIndex];
-		} else if (searchable(array, leftIndex, rightIndex)) {
-			int med = (leftIndex + rightIndex) / 2;
-
-			if (array[med].equals(x) || array[med + 1].equals(x)) {
-				result = x;
-			} else if (array[med + 1].compareTo(x) < 0) {
-				result = floor(array, med + 1, rightIndex, x);
-			} else {
-				result = floor(array, leftIndex, med, x);
-			}
-		}
-
-		return result;
+		return floorOrCeil(array, 0, array.length - 1, x, 1);
 	}
 
 	@Override
 	public Integer ceil(Integer[] array, Integer x) {
-		return ceil(array, 0, array.length - 1, x);
+		return floorOrCeil(array, 0, array.length - 1, x, 0);
 	}
 
-	private Integer ceil(Integer[] array, int leftIndex, int rightIndex, Integer x) {
+	private Integer floorOrCeil(Integer[] array, int leftIndex, int rightIndex, Integer x, int compareOffset) {
 		Integer result = null;
 
 		if (leftIndex == rightIndex) {
@@ -70,10 +34,10 @@ public class FloorCeilBinarySearch implements FloorCeil {
 
 			if (array[med].equals(x) || array[med + 1].equals(x)) {
 				result = x;
-			} else if (array[med].compareTo(x) > 0) {
-				result = ceil(array, leftIndex, med, x);
+			} else if (array[med + compareOffset].compareTo(x) < 0) {
+				result = floorOrCeil(array, med + 1, rightIndex, x, compareOffset);
 			} else {
-				result = ceil(array, med + 1, rightIndex, x);
+				result = floorOrCeil(array, leftIndex, med, x, compareOffset);
 			}
 		}
 
