e2d3f5b70692c96f7191e1579bd4928c9a0dac93
Rob <bruno.robsiq@gmail.com>
2019-05-22 18:58:56 -0300

    Add postOrder test to R11

--- R11-01-Rot-Arvore-Binaria-de-Busca/src/test/java/adt/bst/StudentBSTTest.java
+++ R11-01-Rot-Arvore-Binaria-de-Busca/src/test/java/adt/bst/StudentBSTTest.java
@@ -113,7 +113,9 @@ public class StudentBSTTest {
 
 		Integer[] preOrder = new Integer[] { 6, -34, -40, 5, 2, 0, 23, 9, 12,
 				76, 67, 232 };
-		//assertArrayEquals(preOrder, tree.preOrder());
+		Integer[] postOrder = new Integer[] {-40, 0, 2, 5, -34, 12, 9, 67, 232, 76, 23, 6 };
+		assertArrayEquals(preOrder, tree.preOrder());
+		assertArrayEquals(postOrder, tree.postOrder());
 		assertEquals(4, tree.height());
 
 		tree.remove(0);
